bea3f8552f5b8f5da933d8a8134ba3dab69f37a9
Jessica Bowden <jessica.mbowden@gmail.com>
2025-07-16 15:48:17 -0400

    updates

--- config/.config/nvim/lua/plugins/lsp.lua
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -20,6 +20,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "williamboman/mason.nvim" },
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -40,8 +41,10 @@ return {
   -- LSP Configuration
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
+      "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       "hrsh7th/cmp-nvim-lsp",
     },
@@ -49,101 +52,102 @@ return {
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Setup handlers for mason-lspconfig
-      require("mason-lspconfig").setup_handlers({
-        -- Default handler for all servers
-        function(server_name)
-          lspconfig[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
-
-        -- Custom handlers for specific servers
-        ["ts_ls"] = function()
-          lspconfig.ts_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              typescript = {
-                preferences = {
-                  importModuleSpecifier = "relative",
-                },
+      -- Ensure mason-lspconfig is loaded
+      local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+      if not mason_lspconfig_ok then
+        vim.notify("mason-lspconfig not found", vim.log.levels.ERROR)
+        return
+      end
+      
+      -- Setup LSP servers manually (setup_handlers not available in this version)
+      local servers = {
+        ts_ls = {
+          settings = {
+            typescript = {
+              preferences = {
+                importModuleSpecifier = "relative",
               },
             },
-          })
-        end,
-
-        ["tailwindcss"] = function()
-          lspconfig.tailwindcss.setup({
-            capabilities = capabilities,
-            filetypes = { "html", "css", "scss", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-          })
-        end,
-
-        ["html"] = function()
-          lspconfig.html.setup({
-            capabilities = capabilities,
-            filetypes = { "html", "htmldjango" },
-          })
-        end,
-
-        ["jsonls"] = function()
-          lspconfig.jsonls.setup({
-            capabilities = capabilities,
-            settings = {
-              json = {
-                schemas = require("schemastore").json.schemas(),
-                validate = { enable = true },
+          },
+        },
+        tailwindcss = {
+          filetypes = { "html", "css", "scss", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+        },
+        html = {
+          filetypes = { "html", "htmldjango" },
+        },
+        jsonls = {
+          settings = {
+            json = {
+              schemas = require("schemastore").json.schemas(),
+              validate = { enable = true },
+            },
+          },
+        },
+        eslint = {
+          on_attach = function(client, bufnr)
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              command = "EslintFixAll",
+            })
+          end,
+        },
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+              },
+              telemetry = {
+                enable = false,
               },
             },
-          })
-        end,
+          },
+        },
+        cssls = {},
+        pyright = {},
+      }
 
-        ["eslint"] = function()
-          lspconfig.eslint.setup({
-            capabilities = capabilities,
-            on_attach = function(client, bufnr)
-              vim.api.nvim_create_autocmd("BufWritePre", {
-                buffer = bufnr,
-                command = "EslintFixAll",
-              })
-            end,
-          })
-        end,
-
-        ["lua_ls"] = function()
-          lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
-                },
-                workspace = {
-                  library = vim.api.nvim_get_runtime_file("", true),
-                  checkThirdParty = false,
-                },
-                telemetry = {
-                  enable = false,
-                },
-              },
-            },
-          })
-        end,
-      })
+      -- Setup each server
+      for server_name, server_config in pairs(servers) do
+        local opts = vim.tbl_deep_extend("force", {
+          capabilities = capabilities,
+        }, server_config)
+        
+        lspconfig[server_name].setup(opts)
+      end
     end,
   },
 
   -- TypeScript utilities
   {
-    "jose-elias-alvarez/typescript.nvim",
-    dependencies = { "nvim-lspconfig" },
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lspconfig", "nvim-lua/plenary.nvim" },
     ft = { "typescript", "typescriptreact" },
     config = function()
-      require("typescript").setup({
-        disable_commands = false,
-        debug = false,
-        go_to_source_definition = {
-          fallback = true,
+      require("typescript-tools").setup({
+        settings = {
+          separate_diagnostic_server = true,
+          publish_diagnostic_on = "insert_leave",
+          expose_as_code_action = {},
+          tsserver_path = nil,
+          tsserver_plugins = {},
+          tsserver_max_memory = "auto",
+          tsserver_format_options = {},
+          tsserver_file_preferences = {},
+          tsserver_locale = "en",
+          complete_function_calls = false,
+          include_completions_with_insert_text = true,
+          code_lens = "off",
+          disable_member_code_lens = true,
+          jsx_close_tag = {
+            enable = false,
+            filetypes = { "javascriptreact", "typescriptreact" },
+          },
         },
       })
     end,
